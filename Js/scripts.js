6ff3a34f007179ecc9d2bf6adae87765a446aa9b
danielsind <danielsindet@gmail.com>
2019-12-19 21:23:37 +0300

    change checkbox to buttons

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,5 +1,15 @@
+var balckOliveSize = parseInt(document.getElementsByTagName("select")[0].value);
+var blackOliveCrust =document.getElementsByTagName("select")[1].value
+function blackOlive (){
+    alert(balckOliveSize + blackOliveCrust)
+}
 $(document).ready(function(){
     $("h1").click(function(){
         alert("A tilte")
     });
 })
+function pizza (size,topping,crust){
+    this.size = size
+    this.topping=topping
+    this.crust=crust
+}
