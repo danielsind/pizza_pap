ec79b63d98bfdd8802f34564a3b3dc7682179c7a
danielsind <danielsindet@gmail.com>
2019-12-20 00:49:49 +0300

    allow user to enter his order on the button pres

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,15 +1,20 @@
-var blackOliveCrust =document.getElementById("blackOliveCrust").value;
+
+var sizes = ["Random","Small","Medium","Large"]
+var crustPrizes = [100,200,100,150,200]
+var crustNames = ["Random","Thick","Thin","Deep","Stuffed"]
 function blackOlive (){
-   blackOliveSize = prompt("What size of pizza/n" + " S-Small M-Medium L-Large")
-   alert(blackOliveSize);
+   blackOliveSize = prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)")
+   blackOliveCrust = prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)")
+   blackOliveQuantity = parseInt(prompt("Quantitiy "))
+   alert("Fisrst order is " + blackOliveQuantity + " of " + sizes[blackOliveSize] + " size " + crustNames[blackOliveCrust] + " @" + crustPrizes[blackOliveCrust] + " " )
 }
 $(document).ready(function(){
     $("h1").click(function(){
         alert("A tilte")
     });
 })
-function pizza (size,topping,crust){
+/*function pizza (size,topping,crust){
     this.size = size
     this.topping=topping
     this.crust=crust
-}
+}*/
