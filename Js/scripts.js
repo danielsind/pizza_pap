97582452387362e2988ada5f5e429d0ef80a3316
danielsind <danielsindet@gmail.com>
2019-12-20 02:00:42 +0300

    add new image files that will help in making the page UI awesome

--- Js/scripts.js
+++ Js/scripts.js
@@ -3,6 +3,7 @@ var sizes = ["Random","Small","Medium","Large"]
 var sizePrice = [0,500,700,1000]
 var crustPrizes = [100,200,100,150,200]
 var crustNames = ["Random","Thick","Thin","Deep","Stuffed"]
+ul = "<ul>"
 function blackOlive(){
    blackOliveSize =parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
    blackOliveCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
@@ -29,6 +30,7 @@ function extraCheese(){
     extraCheeseSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
     extraCheeseCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
     extraCheeseQuantity = parseInt(prompt("Quantity?"))
+    
 }
 function pineapple(){
     pineapplesize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
