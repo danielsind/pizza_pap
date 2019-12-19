7692e8a3e9e9d1d79566c31d76668b3d168d8604
danielsind <danielsindet@gmail.com>
2019-12-20 01:28:28 +0300

    add prompt to extracheesse section

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,12 +1,34 @@
 
 var sizes = ["Random","Small","Medium","Large"]
+var sizePrice = [0,500,700,1000]
 var crustPrizes = [100,200,100,150,200]
 var crustNames = ["Random","Thick","Thin","Deep","Stuffed"]
-function blackOlive (){
-   blackOliveSize = prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)")
-   blackOliveCrust = prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)")
+function blackOlive(){
+   blackOliveSize =parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
+   blackOliveCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
    blackOliveQuantity = parseInt(prompt("Quantitiy "))
-   alert("Fisrst order is " + blackOliveQuantity + " of " + sizes[blackOliveSize] + " size " + crustNames[blackOliveCrust] + " @" + crustPrizes[blackOliveCrust] + " " )
+   alert("Order is " + blackOliveQuantity + " of " + sizes[blackOliveSize] + " size " + crustNames[blackOliveCrust] + " crust" + " @" + crustPrizes[blackOliveCrust] + " " )
+}
+function mushroom(){
+    mushroomSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
+    mushroomCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
+    mushroomQuantity = parseInt(prompt("Quantity: "))
+    alert("Order is " + mushroomQuantity + " of " + sizes[mushroomSize] + " size" + crustNames[mushroomCrust] + " crust " + "@" + crustPrizes[mushroomCrust])
+}
+function pepperoni(){
+    pepperoniSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
+    pepperoniCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
+    pepperoniQuantity = parseInt(prompt("Quantity"))
+}
+function onion(){
+    onionSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
+    onionCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
+    onionQuantity = parseInt(prompt("Quantity ?"))
+}
+function extraCheese(){
+    extraCheeseSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
+    extraCheeseCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
+    extraCheeseQuantity = parseInt(prompt("Quantity?"))
 }
 $(document).ready(function(){
     $("h1").click(function(){
