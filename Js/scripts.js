4a03a65251796aeaf72e5744a634e680e68e769e
danielsind <danielsindet@gmail.com>
2019-12-20 03:37:06 +0300

    add some new effect on the listing

--- Js/scripts.js
+++ Js/scripts.js
@@ -3,12 +3,13 @@ var sizes = ["Random","Small","Medium","Large"]
 var sizePrice = [0,500,700,1000]
 var crustPrizes = [100,200,100,150,200]
 var crustNames = ["Random","Thick","Thin","Deep","Stuffed"]
-ul = "<ul>"
 function blackOlive(){
    blackOliveSize =parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
    blackOliveCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
    blackOliveQuantity = parseInt(prompt("Quantitiy "))
+   totalCost = crustPrizes[blackOliveCrust] + sizePrice[blackOliveSize] * blackOliveQuantity
    alert("Order is " + blackOliveQuantity + " of " + sizes[blackOliveSize] + " size " + crustNames[blackOliveCrust] + " crust" + " @" + crustPrizes[blackOliveCrust] + " " )
+   document.getElementById("blackOliveOrder").innerHTML = blackOliveQuantity + " " + sizes[blackOliveSize] + "/" + crustNames[blackOliveCrust] + " crust" + " @" + totalCost
 }
 function mushroom(){
     mushroomSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
@@ -20,6 +21,9 @@ function pepperoni(){
     pepperoniSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
     pepperoniCrust = parseInt(prompt("Crust: " + "1-Thick(Ksh.200) 2-Thin(KSh.100) 3-Deep(Ksh.150) 4-Stuffed(Ksh.200)"))
     pepperoniQuantity = parseInt(prompt("Quantity"))
+    
+    
+
 }
 function onion(){
     onionSize = parseInt(prompt("Size: "+ " 1-Small(Ksh.500) 2-Medium(Ksh.700) 3-Large(Ksh.1000)"))
@@ -41,6 +45,8 @@ $(document).ready(function(){
     $("button").click(function(){
         $("#sadchef").fadeToggle("slow")
         $("#chef2").fadeToggle("slow")
+        $("#deliver").fadeToggle("slow")
+        $("#pick").fadeToggle("slow")
     });
 })
 /*function pizza (size,topping,crust){
